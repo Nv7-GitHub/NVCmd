e652667a0782cab6267c4310f57378f8c03fccee
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-06-26 00:14:07 -0700

    Vidppt

--- vidppt/go.mod
+++ vidppt/go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/Nv7-Github/pptx v0.0.0-20210625170014-50eb87d2576c
+	github.com/Nv7-Github/vidego v0.0.0-20210626064057-5c31c44e6e3b
+	github.com/alexflint/go-arg v1.4.2
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/schollz/progressbar/v3 v3.8.2
-	gocv.io/x/gocv v0.26.0
 )
--- vidppt/main.go
+++ vidppt/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"embed"
-	"flag"
 	"fmt"
 	"image"
 	"io"
 	"os"
 
 	"github.com/Nv7-Github/pptx"
+	"github.com/Nv7-Github/vidego"
+	arg "github.com/alexflint/go-arg"
 	"github.com/nfnt/resize"
-	"gocv.io/x/gocv"
 )
 
-var outFilename = *flag.String("output", "out.pptx", "the output PowerPoint file")
-var in = *flag.String("input", "", "the input video file")
+type Args struct {
+	Input  string `help:"input video file" arg:"-i"`
+	Output string `help:"output video file" arg:"-o"`
+}
 
 const (
 	width  = 960
@@ -24,73 +26,93 @@ const (
 //go:embed blank.pptx
 var blankFS embed.FS
 
+var p *arg.Parser
+
 func handle(err error) {
 	if err != nil {
-		flag.Usage()
-		fmt.Println("error: ", err.Error())
-		os.Exit(2)
+		p.Fail(err.Error())
 	}
 }
 
 func main() {
+	var args Args
+	p = arg.MustParse(&args)
+
+	// Check if file exists
+	_, err := os.Stat(args.Input)
+	if os.IsNotExist(err) {
+		fmt.Println(args.Input)
+		handle(fmt.Errorf("error: file does not exist"))
+	}
+
 	// Open video
-	vid, err := gocv.VideoCaptureFile(in)
+	vid, err := vidego.NewDecoder(args.Input)
 	handle(err)
+	defer vid.Free()
 
 	// Create output file with blank PPTX
 	blank, err := blankFS.Open("blank.pptx")
 	handle(err)
 	defer blank.Close()
 
-	outFile, err := os.Create(outFilename)
+	outFile, err := os.Create(args.Output)
 	handle(err)
 	defer outFile.Close()
 
 	io.Copy(outFile, blank)
 
 	// Open as PPTX
-	out, err := pptx.Open(outFilename)
+	out, err := pptx.Open(args.Output)
 	handle(err)
 
 	// Write to file
-	img := gocv.NewMat()
-	var im image.Image
 	var s pptx.Slide
 
 	// Loop
-	bar := newBar(int64(vid.Get(gocv.VideoCaptureFrameCount)))
-	for vid.Read(&img) {
-		// Decode image
-		if img.Empty() {
+	bar := newBar(int64(vid.FrameCount()))
+	var imgs []image.Image
+	var cont bool
+	for {
+		// Decode frames
+		cont, imgs, err = vid.GetNextFrame()
+		if !cont {
+			break
+		}
+		handle(err)
+		if imgs == nil {
 			continue
 		}
-		im, err = img.ToImage()
-		handle(err)
 
-		// Resize image
-		im = resize.Resize(width, height, im, resize.Bicubic)
+		// Add to ppt
+		for _, im := range imgs {
+			// Resize image
+			im = resize.Resize(width, height, im, resize.Bicubic)
 
-		// Save image to PPT
-		s = pptx.Slide{
-			Images: []pptx.Image{
-				{
-					X: 0,
-					Y: 0,
-					Image: pptx.GoImage{
-						Image: im,
+			// Save image to PPT
+			s = pptx.Slide{
+				Images: []pptx.Image{
+					{
+						X: 0,
+						Y: 0,
+						Image: pptx.GoImage{
+							Image: im,
+						},
 					},
 				},
-			},
-		}
-		err = out.Add(s)
-		handle(err)
+			}
+			err = out.Add(s)
+			handle(err)
 
-		err = bar.Add(1)
-		handle(err)
+			err = bar.Add(1)
+			handle(err)
+		}
 	}
 
-	err = out.Close()
-	handle(err)
 	err = bar.Finish()
 	handle(err)
+
+	fmt.Println("Saving...")
+	err = out.Close()
+	handle(err)
+	fmt.Println("Done!")
 }
